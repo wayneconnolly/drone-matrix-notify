26d176ab83e136d7d6e9177e3c7c2f3a24a80264
Wayne Connolly <wayne@connolly.id.au>
2023-06-01 20:51:08 +1000

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,13 @@ if [[ -n $PLUGIN_SERVER ]]; then
     SERVER="$PLUGIN_SERVER"
 fi
 if [[ -n $PLUGIN_ROOMID ]]; then
-    ROOMID="$PLUGIN_ROOMID"
+    ROOMID="!$PLUGIN_ROOMID"
 fi
 if [[ -n $PLUGIN_TOKEN ]]; then
     TOKEN="$PLUGIN_TOKEN"
 fi
+if [[ -n $PLUGIN_TEMPLATE ]]; then
+    TEMPLATE="$PLUGIN_TEMPLATE"
+fi
 
-bash ./scripts/matrix.sh  --homeserver="${SERVER}" --token="${SERVER}" --room="${SERVER}" "$MSG"
+bash ./scripts/matrix.sh  --homeserver="${SERVER}" --token="${SERVER}" --room="${SERVER}" "$TEMPLATE"
